d46a95317f075016f34c7ab8718974a3b01446fa
Madhav Nandan Singh <madhavnandansingh@gmail.com>
2023-01-24 22:05:21 +0530

    adding sum.cpp in git with command 1.git add sum.cpp 2. git commit -m message in string form

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+    int a , b;
+    cout<<"Enter the first value : "<<endl;
+    cin>>a;
+    cout<<"Enter the Second value : "<<endl;
+    cin>>b;
+    int sum = a+b;
+    cout<<"The sum is : "<<sum;
+    getch();
+}
